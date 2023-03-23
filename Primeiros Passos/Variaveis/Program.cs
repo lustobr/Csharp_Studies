35b351003f1e5560ddf1a6e908ee43b346a6f4f3
Victor <victorm20005@gmail.com>
2023-03-23 11:50:18 -0300

    Atualizando

---
+++ Primeiros Passos/Variaveis/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Csharp_Studies {
+    class ProgramVar {
+        static void Main(string[] args) {
+            int idade = 18;
+
+            Console.WriteLine(idade);
+        }
+    }
+}
