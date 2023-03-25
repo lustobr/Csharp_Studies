443525f6ef5f46cc267fd84d5e4337560f297ca8
Victor <victorm20005@gmail.com>
2023-03-24 23:20:13 -0300

    Pastas organizadas

---
+++ Primeiros Passos/02. Tecla/Tecla.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Csharp_Studies
+{
+    class Tecla
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Digite qualquer tecla:");
+            Console.ReadLine();
+        }
+    }
+}
