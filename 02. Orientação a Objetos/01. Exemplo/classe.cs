5a23a9f889a1be929049e1f34420e02763411189
Victor <victorm20005@gmail.com>
2023-03-26 18:31:33 -0300

    Organizando files

---
+++ 02. Orientação a Objetos/01. Exemplo/classe.cs
@@ -0,0 +1,10 @@
+namespace contaCorrente
+{
+    public class classe
+    {
+        public int numero_agencia;
+        public string conta;
+        public string titular;
+        public double saldo;
+    }
+}
---
+++ 02. Orientação a Objetos/01. Exemplo/main.cs
@@ -0,0 +1,19 @@
+﻿using contaCorrente;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        classe ContaDoVictor = new classe();
+
+        ContaDoVictor.titular = "Victor";
+        ContaDoVictor.numero_agencia = 40;
+        ContaDoVictor.conta = "1010-x";
+        ContaDoVictor.saldo = 50;
+
+        Console.WriteLine(ContaDoVictor.titular);
+        Console.WriteLine(ContaDoVictor.numero_agencia);
+        Console.WriteLine(ContaDoVictor.conta);
+        Console.WriteLine(ContaDoVictor.saldo);
+    }
+}
