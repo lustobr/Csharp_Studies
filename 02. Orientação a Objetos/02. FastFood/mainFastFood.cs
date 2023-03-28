49f5c9337b2a160e9d763c1f65f370b78a1fe756
Victor <victorm20005@gmail.com>
2023-03-28 11:51:17 -0300

    Atualizando mainFastFood

--- 02. Orientação a Objetos/02. FastFood/mainFastFood.cs
+++ 02. Orientação a Objetos/02. FastFood/mainFastFood.cs
@@ -12,9 +12,9 @@ public class MainFastFood
         cliente01.endereço = "Avenida São Paulo 123";
         cliente01.valorTotal = 20.3;
         cliente01.itens = new ItensFastFood();
-        cliente01.itens.iten01 = "Fini";
-        cliente01.itens.iten02 = "Arroz";
-        cliente01.itens.iten03 = "Pneu";
+        cliente01.itens.iten01 = "Hamburguer";
+        cliente01.itens.iten02 = "Batata";
+        cliente01.itens.iten03 = "Refri";
 
         Console.WriteLine("O Cliente {0}, mora na {1} tem os seguintes itens: \n{2}\n{3}\n{4}\nValor total: {5}", cliente01.cliente, cliente01.endereço, cliente01.itens.iten01, cliente01.itens.iten02, cliente01.itens.iten03, cliente01.valorTotal);
 
@@ -22,9 +22,9 @@ public class MainFastFood
         Output:
 
         "O Cliente Victor, mora na Avenida São Paulo 123 tem os seguintes itens: 
-        Fini
-        Arroz 
-        Pneu
+        Hamburger
+        Batata
+        Refri
         Valor total: 20,3"
         */
     }
